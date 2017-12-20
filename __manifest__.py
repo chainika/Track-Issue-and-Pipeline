43d97a1caa6088b1b8bf036ed15c267e2f0abdfb
chainika <chainikasinghal@gmail.com>
2017-12-20 13:24:52 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+'name':'Task and Issue from Pipeline',
+'version':'1.0',
+'author':'DiracERP Solutions',
+'depends':['crm','project','project_issue'],                                                 
+'category':'',
+'description':"This Module will give you the fuctionality to track and create the task and issue from Opportunity. Apart of that it will give you the track of activities from smart button",
+
+'data':[
+'view/crm_activity_report.xml',        
+
+        'view/project_task.xml',
+        
+        'view/project_issue.xml',
+        'view/crm_lead.xml',
+        ],
+'website':'www.diracerp.com',
+'demo_xml':[],
+'js':[],
+'css':[],
+
+'installable':True,
+'auto_install':False,
+'active':False,
+}
